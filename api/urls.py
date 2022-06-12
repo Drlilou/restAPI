1e1f1ef9582f163089e011b6d146e52dd853348a
Boudjema Ali <boudjema.ali94@gmail.com>
2022-06-12 23:26:27 +0200

    views and urls (api)

---
+++ api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path,include
+from . import views
+
+from rest_framework.urlpatterns import format_suffix_patterns
+urlpatterns = [
+    
+  
+    path('accounts/signup/client/', views.ClientSignupView.as_view()),#post
+    path('accounts/signup/driver/', views.DriverSignupView.as_view()),#post
+    path('accounts/login/'        , views.CustomAuthToken.as_view()),#post
+    path('accounts/logout/'       , views.LogoutView.as_view()),#post
+    path('clients/<pk>/'          ,views.getClient)
+   
+]
+
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,80 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from .models import Client,User
 # Create your views here.
+
+class ClientSignupView(generics.GenericAPIView):
+    serializer_class=ClientSignupSerializer
+    def post(self, request, *args, **kwargs):
+
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user=serializer.save()
+        return Response({
+            "user":UserSerializer(user, context=self.get_serializer_context()).data,
+            #"token":Token.objects.get(user=user).key,
+            "message":"account created successfully"
+        })
+
+class DriverSignupView(generics.GenericAPIView):
+    serializer_class=DriverSignupSerializer
+    def post(self, request, *args, **kwargs):
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user=serializer.save()
+        return Response({
+            "user":UserSerializer(user, context=self.get_serializer_context()).data,
+            #"token":Token.objects.get(user=user).key,
+            "message":"account created successfully"
+        })
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer=self.serializer_class(data=request.data, context={'request':request})
+       
+        serializer.is_valid(raise_exception=True)
+
+        user=serializer.validated_data['user']
+        
+        user.firebaseID=request.data['firebaseID']
+        user.is_connected=True
+        user.save()
+        #token, created=Token.objects.get_or_create(user=user)
+        print(user)
+        return Response({
+            #'token':token.key,
+            'user_id':user.pk,
+            'typeCompte':user.typeCompte,
+            'connection':user.is_connected,
+            'firebaseID':user.firebaseID
+
+        })
+
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        request.auth.delete()
+        serializer=self.serializer_class(data=request.data, context={'request':request})
+       
+        serializer.is_valid(raise_exception=True)
+
+        user=serializer.validated_data['user']
+        
+        user.firebaseID=''
+        user.is_connected=False
+        user.save()
+        
+        return Response(status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def getClient(request,pk):
+    try:
+        client = Client.objects.get(id=pk)
+
+        serializer = ClientSerializer(client,many=False)
+        return Response(serializer.data)
+    except Exception as err :
+        return Response({"id":"wrong user id {}".format(err)})
