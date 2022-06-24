3eec04cbd13d223f8d9a9225bd986eb16ea012fb
Boudjema Ali <boudjema.ali94@gmail.com>
2022-06-24 11:03:40 +0200

    Update notification_sender.py

--- api/notification_sender.py
+++ api/notification_sender.py
@@ -16,19 +16,19 @@ class notifyDriver(APIView):
     def post(self, request):
         #data = json.loads(json.dumps(request.data))
         data={}
-        data['client'] = Client.objects.get(id=request.data['client'])
-        data['driver'] = Driver.objects.get(id=request.data['driver'])#firebase ID
-        data['coursa'] = Coursa.objects.get(id=request.data['coursa'])
+        data["client"] = Client.objects.get(id=request.data["client"])
+        data["driver"] = Driver.objects.get(id=request.data["driver"])#firebase ID
+        data["coursa"] = Coursa.objects.get(id=request.data["coursa"])
         #message=coursa
         response = {}
 
-        if SendToDriver.send_message_to_driver(data['client'].id,
-        			        data['driver'].user.firebaseID,
-                            str(data['coursa'])
+        if SendToDriver.send_message_to_driver(data["client"].id,
+        			        data["driver"].user.firebaseID,
+                            str(data["coursa"])
                       		):
-            response['success'] = True
+            response["success"] = True
         else:
-            response['success'] = False
+            response["success"] = False
 
         return JsonResponse(response)
 
@@ -38,24 +38,24 @@ class SendToDriver(APIView):
     @staticmethod
     def send_message_to_driver(from_client, to_driver, message):
         finalmsg = {
-            'to':  to_driver,
-            'data': {
-                'message': message,
-                'from_user': from_client,
+            "to":  to_driver,
+            "data": {
+                "message": message,
+                "from_user": from_client,
             }
         }
 
         headers = {
-            'Authorization': 'key=' + serverToken,
-            'Content-Type': "application/json",
+            "Authorization": "key=" + serverToken,
+            "Content-Type": "application/json",
         }
 
-        fcm_response = requests.post('https://fcm.googleapis.com/fcm/send', json.dumps(finalmsg), headers=headers)
+        fcm_response = requests.post("https://fcm.googleapis.com/fcm/send", json.dumps(finalmsg), headers=headers)
         print(fcm_response) 
         print("**************")
         fcm_json_response = json.loads(fcm_response.content)
 
-        if fcm_json_response.has_key('error'):
+        if fcm_json_response.has_key("error"):
             return False
         else:
             return True
