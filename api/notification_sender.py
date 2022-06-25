4b4ce8b266ad1131f17dfd028102197dd4b8881c
Boudjema Ali <boudjema.ali94@gmail.com>
2022-06-25 08:47:36 +0200

    notification

--- api/notification_sender.py
+++ api/notification_sender.py
@@ -11,18 +11,19 @@ import json
 
 from django.http import HttpResponse
 import requests
+from rest_framework.decorators import api_view
 
 
 from django.views.decorators.csrf import csrf_exempt
 key1='key=AAAAvaxaZBI:APA91bFIqn723wmyXXBfbRHQ089WfOH1kCtHiwb58XZ0b1maZC42aG61cb8YFv2kPZ_TVQ7VAfqYmhqyZ7kNOLap_jYCHMX5M1mdMosT9-w0zTjQKLd6y8IZ98fMLQmKTCmWcb_fKvlo'
 key ='key=AAAAlQ1Lrfw:APA91bHvI2-qFZNCf-oFfeZgM0JUDxxbuykH_ffka9hPUE0xBpiza4uHF0LmItT_SfMZ1Zl5amGUfAXigaR_VcMsEArqpOwHNup4oRTQ24htJ_GWYH0OWZzFrH2lRY24mnQ-uiHgLyln'
     
-#@csrf_exempt
+@api_view(["POST"])
 def notification(request):
-    print(request.POST)
-
-    recipient =request.POST['firebaseID']#'Testing1'
-    return HttpResponse(recipient)
+    print(request)
+    #return HttpResponse(request.data)
+    recipient =request.data['firebaseID']#'Testing1'
+    #return Response(recipient)
     title='notification'
     message = 'corp de notification'
     url = 'https://fcm.googleapis.com/fcm/send'
