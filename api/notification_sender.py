fcfeb13a96eaa09001c4f213c408233d9a42cdc8
Boudjema Ali <boudjema.ali94@gmail.com>
2022-06-25 22:50:10 +0200

    set up notfication :D

--- api/notification_sender.py
+++ api/notification_sender.py
@@ -15,9 +15,9 @@ from rest_framework.decorators import api_view
 
 
 from django.views.decorators.csrf import csrf_exempt
-key1='key=AAAAvaxaZBI:APA91bFIqn723wmyXXBfbRHQ089WfOH1kCtHiwb58XZ0b1maZC42aG61cb8YFv2kPZ_TVQ7VAfqYmhqyZ7kNOLap_jYCHMX5M1mdMosT9-w0zTjQKLd6y8IZ98fMLQmKTCmWcb_fKvlo'
-key ='key=AAAAlQ1Lrfw:APA91bHvI2-qFZNCf-oFfeZgM0JUDxxbuykH_ffka9hPUE0xBpiza4uHF0LmItT_SfMZ1Zl5amGUfAXigaR_VcMsEArqpOwHNup4oRTQ24htJ_GWYH0OWZzFrH2lRY24mnQ-uiHgLyln'
-    
+#key1='key=AAAAvaxaZBI:APA91bFIqn723wmyXXBfbRHQ089WfOH1kCtHiwb58XZ0b1maZC42aG61cb8YFv2kPZ_TVQ7VAfqYmhqyZ7kNOLap_jYCHMX5M1mdMosT9-w0zTjQKLd6y8IZ98fMLQmKTCmWcb_fKvlo'
+#key ='key=AAAAlQ1Lrfw:APA91bHvI2-qFZNCf-oFfeZgM0JUDxxbuykH_ffka9hPUE0xBpiza4uHF0LmItT_SfMZ1Zl5amGUfAXigaR_VcMsEArqpOwHNup4oRTQ24htJ_GWYH0OWZzFrH2lRY24mnQ-uiHgLyln'
+key ="key=AAAAjllUT98:APA91bFxzSxT05hbe-PUMBuTN7a2FdmUJZIACFzUYRxoyW6Jr-fcx5XpbmDSam8JzXHgtAqj2PFOpSuWeRRnI1mhvSnH4hr_K_L4Y-_O2L7XSGOGow_Y4QKnQzyvFt9EoG091Qr-exP3"    
 @api_view(["POST"])
 def notification(request):
     print(request)
@@ -37,7 +37,7 @@ def notification(request):
     }
     response = requests.post(url, headers=headers, data=json.dumps(data))
     print(response.text)
-    return Response({"no prblm":response.text})
+    return Response({"output":response.text})
 
 
 
